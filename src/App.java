d318665bfbe8dc37a4a4decf4c092ae32c0bdd92
Filipe Giroleti <filipe.fuentes@edu.pucrs.br>
2023-05-26 09:56:21 -0300

    added mostraMenu method

--- src/App.java
+++ src/App.java
@@ -22,6 +22,23 @@ public class App {
     }
 
     public void executa() {
+        mostraMenu();
+    }
 
+    private void mostraMenu() {
+        System.out.println("Menu:");
+        System.out.println("1. Cadastrar novo porto");
+        System.out.println("2. Cadastrar novo navio");
+        System.out.println("3. Cadastrar novo cliente");
+        System.out.println("4. Cadastrar novo tipo de carga");
+        System.out.println("5. Cadastrar nova carga");
+        System.out.println("6. Consultar todas as cargas");
+        System.out.println("7. Alterar a situação de uma carga");
+        System.out.println("8. Carregar dados iniciais");
+        System.out.println("9. Fretar cargas");
+        System.out.println("10. Salvar dados");
+        System.out.println("11. Carregar dados");
+        System.out.println("12. Finalizar sistema");
+        System.out.print("Escolha uma opção: ");
     }
 }
