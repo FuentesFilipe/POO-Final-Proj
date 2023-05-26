8b72d5db8325b56392b093c79b9c3ed6a267ba5d
Filipe Giroleti <filipe.fuentes@edu.pucrs.br>
2023-05-26 09:22:04 -0300

    reorganized project, actually this time

---
+++ src/Duravel.java
@@ -0,0 +1,24 @@
+public class Duravel extends TipoCarga {
+    private String setor;
+    private String materialPrincipal;
+    private double percentIpi;
+
+    public Duravel(int numero, String descricao, String setor, String materialPrincipal, double percentIpi) {
+        super(numero, descricao);
+        this.setor = setor;
+        this.materialPrincipal = materialPrincipal;
+        this.percentIpi = percentIpi;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public String getMaterialPrincipal() {
+        return materialPrincipal;
+    }
+
+    public double getPercentIpi() {
+        return percentIpi;
+    }
+}
